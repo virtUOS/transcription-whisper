a070f65b4dcdc23dcc2a9d5088927106294e75eb
odrec <renatogarita@gmail.com>
2024-09-04 16:35:20 +0200

    Add display of video or audio next to the transcription. Added edit of vtt file as well. Define temp path for files in .env file

--- app.py
+++ app.py
@@ -20,8 +20,9 @@ st.set_page_config(
 
 API_URL = os.getenv("API_URL")
 FFMPEG_PATH = os.getenv("FFMPEG_PATH")
+TEMP_PATH = os.getenv("TEMP_PATH")
 
-base_temp_dir = os.path.expanduser("~/transcription-whisper-temp")
+base_temp_dir = os.path.expanduser(TEMP_PATH)
 os.makedirs(base_temp_dir, exist_ok=True)
 
 
@@ -105,14 +106,13 @@ def process_youtube_link(youtube_link):
     temp_output_path = f"{os.path.splitext(downloaded_file_path)[0]}.mp3"
 
     original_file_name = f"{get_youtube_video_id(youtube_link)}{os.path.splitext(downloaded_file_path)[1]}"
-    return temp_output_path, original_file_name
+    return temp_output_path, original_file_name, os.path.abspath(
+        downloaded_file_path)  # Also return the downloaded file path
 
 
 st.title("Transcription Service")
 
-left_col, divider_col, right_col = st.columns([2, 0.2, 8])
-
-with left_col:
+with st.sidebar:
     st.write("Upload a video or audio file or provide a YouTube link to get a transcription.")
 
     input_type = st.radio("Choose input type", ["Upload File", "YouTube Link"])
@@ -139,127 +139,124 @@ with left_col:
         st.session_state.status = None
     if "original_file_name" not in st.session_state:
         st.session_state.original_file_name = None
+    if "media_file_data" not in st.session_state:
+        st.session_state.media_file_data = None
+    if "input_type" not in st.session_state:
+        st.session_state.input_type = None
 
-    if (uploaded_file or youtube_link) and st.button("Transcribe"):
-        if uploaded_file:
-            input_path, unique_file_path, original_file_name = process_uploaded_file(uploaded_file)
-        elif youtube_link:
-            unique_file_path, original_file_name = process_youtube_link(youtube_link)
+    transcribe_button_clicked = st.button("Transcribe")
 
-        st.session_state.result = None  # Reset previous results
+conversion_placeholder = st.empty()  # Placeholder for conversion message
+upload_placeholder = st.empty()  # Placeholder for upload message
 
-        with right_col:
-            if uploaded_file and os.path.splitext(uploaded_file.name)[1].lower() != '.mp3':
-                st.info("Converting file to mp3...")
-                input_path, unique_file_path, original_file_name = process_uploaded_file(uploaded_file)
-                convert_audio(input_path, unique_file_path)
+if (uploaded_file or youtube_link) and transcribe_button_clicked:
+    if uploaded_file:
+        input_path, unique_file_path, original_file_name = process_uploaded_file(uploaded_file)
+        st.session_state.media_file_data = uploaded_file  # Store media file data
 
-            st.info("Uploading file...")
+    elif youtube_link:
+        unique_file_path, original_file_name, downloaded_file_path = process_youtube_link(youtube_link)
+        with open(downloaded_file_path, "rb") as f:
+            st.session_state.media_file_data = f.read()  # Store media file data
 
-            with open(unique_file_path, "rb") as file_to_transcribe:
-                upload_response = upload_file(file_to_transcribe, lang, model, min_speakers, max_speakers)
+    st.session_state.input_type = input_type  # Store the type of input
 
-            task_id = upload_response.get("task_id")
-            if task_id:
-                st.session_state.task_id = task_id
-                st.session_state.original_file_name = original_file_name
-                st.info(f"File uploaded. Tracking task with ID: {task_id}")
+    st.session_state.result = None  # Reset previous results
+
+    if uploaded_file and os.path.splitext(uploaded_file.name)[1].lower() != '.mp3':
+        conversion_placeholder.info("Converting file to mp3...")
+        input_path, unique_file_path, _ = process_uploaded_file(uploaded_file)
+        convert_audio(input_path, unique_file_path)
+
+    upload_placeholder.info("Uploading file...")
+
+    with open(unique_file_path, "rb") as file_to_transcribe:
+        upload_response = upload_file(file_to_transcribe, lang, model, min_speakers, max_speakers)
+
+    task_id = upload_response.get("task_id")
+    if task_id:
+        st.session_state.task_id = task_id
+        st.session_state.original_file_name = original_file_name
+        upload_placeholder.info(f"File uploaded. Tracking task with ID: {task_id}")
 
 # Process existing task_id from session state
 if st.session_state.task_id and st.session_state.status != "SUCCESS":
-    with divider_col:
-        st.html(
-            '''
-                <div class="divider-vertical-line"></div>
-                <style>
-                    .divider-vertical-line {
-                        border-left: 2px solid rgba(49, 51, 63, 0.2);
-                        height: 320px;
-                        margin: auto;
-                    }
-                </style>
-            '''
-        )
-    with right_col:
-        st.info("Transcription is in progress. Please wait...")
+    st.info("Transcription is in progress. Please wait...")
 
-        status_placeholder = st.empty()
-        start_time = time.time()
+    status_placeholder = st.empty()
+    start_time = time.time()
 
-        while True:
-            status = check_status(st.session_state.task_id)
-            elapsed_time = time.time() - start_time
-            minutes, seconds = divmod(elapsed_time, 60)
+    while True:
+        status = check_status(st.session_state.task_id)
+        elapsed_time = time.time() - start_time
+        minutes, seconds = divmod(elapsed_time, 60)
 
-            if status['status'] == "SUCCESS":
-                st.session_state.status = "SUCCESS"
-                st.session_state.result = status.get('result', {})
-                break
-            elif status['status'] == "FAILURE":
-                st.session_state.status = "FAILURE"
-                st.error(f"Transcription failed. Error: {status.get('error', 'Unknown error')}")
-                break
-            else:
-                st.session_state.status = status['status']
-                status_placeholder.info(
-                    f"Task Status: {status['status']}. Elapsed time: {int(minutes)} min {int(seconds)} sec. Checking again in 30 seconds..."
-                )
-                time.sleep(30)
+        if status['status'] == "SUCCESS":
+            st.session_state.status = "SUCCESS"
+            st.session_state.result = status.get('result', {})
+            break
+        elif status['status'] == "FAILURE":
+            st.session_state.status = "FAILURE"
+            st.error(f"Transcription failed. Error: {status.get('error', 'Unknown error')}")
+            break
+        else:
+            st.session_state.status = status['status']
+            status_placeholder.info(
+                f"Task Status: {status['status']}. Elapsed time: {int(minutes)} min {int(seconds)} sec. Checking again in 30 seconds..."
+            )
+            time.sleep(30)
 
 # Display result if transcription is successful
 if st.session_state.status == "SUCCESS" and st.session_state.result:
-    with right_col:
-        st.success("Transcription successful!")
+    st.success("Transcription successful!")
 
-        base_name = os.path.splitext(st.session_state.original_file_name)[0]
+    base_name = os.path.splitext(st.session_state.original_file_name)[0]
 
-        result = st.session_state.result
-        vtt_content = result.get('vtt_content', "")
-        txt_content = result.get('txt_content', "")
-        json_content = result.get('json_content', "")
-        srt_content = result.get('srt_content', "")
+    result = st.session_state.result
 
-        button1_col, button2_col, button3_col, button4_col = st.columns(4)
+    # Default to empty string if any field is None
+    vtt_content = result.get('vtt_content') or ""
+    txt_content = result.get('txt_content') or ""
+    json_content = result.get('json_content') or ""
+    srt_content = result.get('srt_content') or ""
 
-        button1_col.download_button(label="Download VTT File",
-                                    data=BytesIO(vtt_content.encode('utf-8')) if vtt_content else BytesIO(),
-                                    file_name=f"{base_name}.vtt",
-                                    mime="text/vtt")
+    button1_col, button2_col, button3_col, button4_col = st.columns(4)
 
-        button2_col.download_button(label="Download TXT File",
-                                    data=BytesIO(txt_content.encode('utf-8')) if txt_content else BytesIO(),
-                                    file_name=f"{base_name}.txt",
-                                    mime="text/plain")
+    button1_col.download_button(label="Download VTT File",
+                                data=BytesIO(vtt_content.encode('utf-8')),
+                                file_name=f"{base_name}.vtt",
+                                mime="text/vtt")
 
-        button3_col.download_button(label="Download JSON File",
-                                    data=BytesIO(json_content.encode('utf-8')) if json_content else BytesIO(),
-                                    file_name=f"{base_name}.json",
-                                    mime="application/json")
+    button2_col.download_button(label="Download TXT File",
+                                data=BytesIO(txt_content.encode('utf-8')),
+                                file_name=f"{base_name}.txt",
+                                mime="text/plain")
 
-        button4_col.download_button(label="Download SRT File",
-                                    data=BytesIO(srt_content.encode('utf-8')) if srt_content else BytesIO(),
-                                    file_name=f"{base_name}.srt",
-                                    mime="text/srt")
+    button3_col.download_button(label="Download JSON File",
+                                data=BytesIO(json_content.encode('utf-8')),
+                                file_name=f"{base_name}.json",
+                                mime="application/json")
 
-        st.write("Transcription Result:")
-        selected_tab = st.selectbox("Select format to view/edit", ["txt", "json", "srt"])
+    button4_col.download_button(label="Download SRT File",
+                                data=BytesIO(srt_content.encode('utf-8')),
+                                file_name=f"{base_name}.srt",
+                                mime="text/srt")
 
-        # Add custom CSS to set max height for the Quill editor
-        st.markdown("""
-            <style>
-                .ql-editor {
-                    max-height: 400px;
-                    overflow-y: auto;
-                }
-            </style>
-        """, unsafe_allow_html=True)
+    st.write("Transcription Result:")
 
+    # Create columns for the media and editor
+    media_col, editor_col = st.columns([1, 2])  # Adjust the ratio as needed
+
+    with editor_col:
+        selected_tab = st.selectbox("Select format to view/edit", ["txt", "json", "srt", "vtt"])
         if selected_tab == "txt":
             edited_content = st_quill(value=txt_content, key="txt_edit")
         elif selected_tab == "json":
             edited_content = st_quill(value=json_content, key="json_edit")
         elif selected_tab == "srt":
             edited_content = st_quill(value=srt_content, key="srt_edit")
+        elif selected_tab == "vtt":
+            edited_content = st_quill(value=vtt_content, key="vtt_edit")
 
         # Update session state with edited content
         if 'txt_edit' in st.session_state:
@@ -268,3 +265,21 @@ if st.session_state.status == "SUCCESS" and st.session_state.result:
             st.session_state.result['json_content'] = st.session_state['json_edit']
         elif 'srt_edit' in st.session_state:
             st.session_state.result['srt_content'] = st.session_state['srt_edit']
+        elif 'vtt_edit' in st.session_state:
+            st.session_state.result['vtt_content'] = st.session_state['vtt_edit']
+
+    with media_col:
+        if st.session_state.media_file_data:
+            ext = os.path.splitext(st.session_state.original_file_name)[1].lower()
+
+            if st.session_state.input_type == "Upload File":
+                if ext in ['.mp3', '.wav']:
+                    st.audio(st.session_state.media_file_data)
+                elif ext in ['.mp4']:
+                    st.video(st.session_state.media_file_data)
+            else:
+                # For downloaded files
+                if ext in ['.mp3', '.wav']:
+                    st.audio(BytesIO(st.session_state.media_file_data))
+                elif ext in ['.mp4']:
+                    st.video(BytesIO(st.session_state.media_file_data))
