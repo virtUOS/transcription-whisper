ab8351fbb89a4f99a02315ac13711a5c985426fa
Dennis Benz <dennis.benz@uni-osnabrueck.de>
2024-12-16 14:31:43 +0100

    Add docker entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# Keep package yt-dlp updated on container start up
+pip install --no-cache-dir yt-dlp
+
+# Run with streamlit user for security
+runuser -u streamlituser -- streamlit run app.py --server.port=8501 --server.address=0.0.0.0 "$@"
