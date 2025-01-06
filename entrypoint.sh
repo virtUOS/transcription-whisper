c2e3ec8871440e48c4a700448c162cd50f24b71a
Dennis Benz <dennis.benz@uni-osnabrueck.de>
2025-01-06 14:23:45 +0100

    Add upgrade option to pip install

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Keep package yt-dlp updated on container start up
-pip install --no-cache-dir yt-dlp
+pip install --upgrade --no-cache-dir yt-dlp
 
 # Run with streamlit user for security
 runuser -u streamlituser -- streamlit run app.py --server.port=8501 --server.address=0.0.0.0 "$@"
